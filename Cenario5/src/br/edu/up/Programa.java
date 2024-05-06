c3df94d31dde58e09c31ebe897896b153c5d9eec
Brian Snack <snackbrian10@gmail.com>
2024-05-06 09:44:54 -0300

    Cenario4 pronto e criacação dos outros cenários

---
+++ Cenario5/src/br/edu/up/Programa.java
@@ -0,0 +1,15 @@
+package br.edu.up;
+
+import java.util.Scanner;
+
+public class Programa {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+
+
+
+
+
+        leitor.close();
+    }
+}
