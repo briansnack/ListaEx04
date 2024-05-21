85581d7f09b0e4bf9ad97effe7602cf85bc8b77f
Brian Snack <122538036+briansnack@users.noreply.github.com>
2024-05-20 22:40:09 -0300

    Cenario10 e correção de nomes de arquivos no Cenário9

--- Cenario10/src/br/edu/up/Programa.java
+++ Cenario10/src/br/edu/up/Programa.java
@@ -3,7 +3,7 @@ package br.edu.up;
 import br.edu.up.controller.SeguroController;
 import br.edu.up.view.SeguroView;
 
-public class Main {
+public class Programa {
     public static void main(String[] args) {
         SeguroView view = new SeguroView();
         SeguroController controller = new SeguroController(view);
